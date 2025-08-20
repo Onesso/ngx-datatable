b37234dfa525f1e08f6832f5f406845f0fbe0b99
Onesso <odhiambofrank1965@gmail.com>
2025-08-20 09:19:39 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { rows } from '../data';
+import { columns } from '../data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxDatatableModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('first-ngx-datatable');
+  rows = rows;
+  columns = columns;
 }
